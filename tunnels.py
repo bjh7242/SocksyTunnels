d7a8c2f5021c18f57a057fb88b2f240edc6e9eeb
bjh7242 <bjh7242@rit.edu>
2018-04-15 11:57:04 -0400

    http requests work through the tunnel

--- tunnels.py
+++ tunnels.py
@@ -3,16 +3,50 @@
 # proxy list
 # https://www.sslproxies.org
 
-import socks
 import argparse
+import socket
+import socks
+import sys
 
 
-""" my header 
-to Proxy IP:PORT through socks proxy
+def create_listener(listenip, listenport, socksconn):
+    """
+    args:     listenip - interface to listen on
+              listenport - port to listen on
+              socksconn - socks.socksocket object with an established connection
+    """
+    # create socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind((listenip, listenport))
+        s.listen(1)            # listen with 1 queued connection
+    except socket.error as msg:
+        s.close()
+        s = None
+        print msg
+        sys.exit()
 
-CONNECT https://google.com
+    while True:
+        response = ""
+        try:
+            conn, addr = s.accept()
+            print "Connection from address: " + str(addr)
+            req = conn.recv(1024)
+            if not req:
+                break
+            print req
+            socksconn.send(req)		# send the data through the socks connection
+            while True:
+                data = socksconn.recv(1024)
+                if not data:
+                    break
+                conn.send(data)
 
-"""
+        except KeyboardInterrupt:
+            print "CTRL-C received. Quitting."
+            break
+    sys.exit()
+    
 
 def create_tunnel(args):
     socksproxy = args.socks_proxy
@@ -21,27 +55,31 @@ def create_tunnel(args):
     sslproxyport = int(args.ssl_proxy_port)
     destination = args.destination_ip
     destinationport = int(args.destination_port)
+    listenport = int(args.listen_port)
+    listenip = args.listen_ip
     
-    sock = socks.socksocket() # Same API as socket.socket in the standard lib
-    sock.set_proxy(socks.SOCKS5, socksproxy, socksport)	# tor entrance
+    sock = socks.socksocket() 
+    sock.set_proxy(socks.SOCKS5, socksproxy, socksport)    # tor entrance
     
-    sock.connect((sslproxy, sslproxyport))
-    #sock.send('GET / HTTP/1.1\r\nHost: google.com\r\n\r\n')
+    sock.connect((sslproxy, sslproxyport))    # connect to the proxy after exiting tor
+    # send CONNECT request to the proxy
     sock.send('CONNECT ' + destination + ':' + str(destinationport) + ' HTTP/1.1\r\n\r\n')
     
-    print sock.recv(5000)
+    print sock.recv(1024)
+    create_listener(listenip, listenport, sock)
     
-    sock.send('GET / HTTP/1.1\r\nHost: ' + destination + '\r\n\r\n')
-    
-    print sock.recv(5000)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--socks-proxy', '-s', help='IP of the SOCKS proxy (first hop)', required=True)
     parser.add_argument('--socks-port', '-x', help='Port of the SOCKS proxy (first hop)', required=True)
-    parser.add_argument('--ssl-proxy', '-l', help='IP of the last hop (exit proxy)', required=True)
+    parser.add_argument('--ssl-proxy', '-o', help='IP of the last hop (exit proxy)', required=True)
     parser.add_argument('--ssl-proxy-port', '-r', help='Port of the ssl proxy', required=True)
     parser.add_argument('--destination-ip', '-d', help='IP of the ultimate destination of the tunnel', required=True)
     parser.add_argument('--destination-port', '-p', help='Port of the destination of the tunnel', required=True)
+    parser.add_argument('--listen-port', '-l', help='Port to listen on on the computer you are running this on. This will be forwarded to the destination port', required=True)
+    parser.add_argument('--listen-ip', '-n', help='Interface to listen on (default=0.0.0.0)', default='0.0.0.0', required=False)
     args = parser.parse_args()
     create_tunnel(args)
+
+
