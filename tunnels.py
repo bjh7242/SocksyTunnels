7ca8115e1e34fc26edeb5379f8c79a5cee658412
bjh7242 <bjh7242@rit.edu>
2018-04-15 13:25:23 -0400

    tunnels working, but slow afffff

--- tunnels.py
+++ tunnels.py
@@ -7,6 +7,33 @@ import argparse
 import socket
 import socks
 import sys
+import time
+
+def recv_timeout(the_socket,timeout=2):
+    the_socket.setblocking(0)
+    total_data = []
+    data = '' 
+    begin = time.time()
+
+    while 1:
+        #if you got some data, then break after wait sec
+        if total_data and time.time() - begin > timeout:
+            break
+        #if you got no data at all, wait a little longer
+        elif time.time()-begin>timeout*2:
+            break
+
+        try:
+            data=the_socket.recv(8192)
+            if data:
+                total_data.append(data)
+                begin=time.time()
+            else:
+                time.sleep(0.1)
+        except:
+            pass
+
+    return ''.join(total_data)
 
 
 def create_listener(listenip, listenport, socksconn):
@@ -17,6 +44,8 @@ def create_listener(listenip, listenport, socksconn):
     """
     # create socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #s.setblocking(0)
+    #socksconn.setblocking(0)
     try:
         s.bind((listenip, listenport))
         s.listen(1)            # listen with 1 queued connection
@@ -26,25 +55,27 @@ def create_listener(listenip, listenport, socksconn):
         print msg
         sys.exit()
 
-    while True:
-        response = ""
-        try:
-            conn, addr = s.accept()
-            print "Connection from address: " + str(addr)
-            req = conn.recv(1024)
-            if not req:
-                break
+    try:
+        conn, addr = s.accept()
+        print "Connection from address: " + str(addr)
+        while True:
+            #req = conn.recv(1024)
+            req = recv_timeout(conn)
+            #if not req:
+            #    break
             print req
-            socksconn.send(req)		# send the data through the socks connection
-            while True:
-                data = socksconn.recv(1024)
-                if not data:
-                    break
-                conn.send(data)
+            socksconn.sendall(req)		# send the data through the socks connection
+            #while True:
+            #data = socksconn.recv(1024)
+            data = recv_timeout(socksconn)
+            print data
+            if not data:
+                break
+            conn.sendall(data)
 
-        except KeyboardInterrupt:
-            print "CTRL-C received. Quitting."
-            break
+    except KeyboardInterrupt:
+        print "CTRL-C received. Quitting."
+        #break
     sys.exit()
     
 
